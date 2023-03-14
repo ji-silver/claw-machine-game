87e9577606b317108d82b71165604e41a6e59f65
jisilver <jisilver.kim@gmail.com>
2023-03-14 22:33:20 +0900

    feat: 인형 랜덤배치

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+const dollsContainer = document.getElementById("dolls-container");
+
+// 인형 이미지 경로 배열
+const dollImages = ["./img/doll1.png", "./img/doll2.png", "./img/doll3.png", "./img/bomb.png"];
+
+
+function RandomDolls() {
+
+  dollsContainer.innerHTML = "";
+
+  const dollCount = 36; // 인형 갯수
+
+  for (let i = 0; i < dollCount; i++) { // 인형 생성하기
+    const dollImg = document.createElement("img");
+
+    dollImg.src = dollImages[Math.floor(Math.random() * dollImages.length)]; // 랜덤 생성
+
+    dollImg.classList.add("dolls");
+    dollsContainer.appendChild(dollImg);
   }
-  
-  const dollsContainer = document.querySelector('#dolls-container');
-  const dolls = Array.from(dollsContainer.querySelectorAll('img'));
-  const shuffledDolls = shuffle(dolls);
-  
-  dollsContainer.innerHTML = '';
-  shuffledDolls.forEach(doll => {
-    doll.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`;
-    doll.style.transformOrigin = 'center';
-    dollsContainer.appendChild(doll);
-  });
+}
+
+RandomDolls();
