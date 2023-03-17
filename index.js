dcad8ceb674c0e8c13a170e36f5bab7ec5e3d723
jisilver <jisilver.kim@gmail.com>
2023-03-17 22:02:41 +0900

    feat: 인형 랜덤 배치

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const dollsContainer = document.getElementById("dolls-container");
-
-// 인형 이미지 경로 배열
-const dollImages = ["./img/doll1.png", "./img/doll2.png", "./img/doll3.png", "./img/bomb.png"];
-
-
-function RandomDolls() {
-
-  dollsContainer.innerHTML = "";
-
-  const dollCount = 36; // 인형 갯수
-
-  for (let i = 0; i < dollCount; i++) { // 인형 생성하기
-    const dollImg = document.createElement("img");
-
-    dollImg.src = dollImages[Math.floor(Math.random() * dollImages.length)]; // 랜덤 생성
-
-    dollImg.classList.add("dolls");
-    dollsContainer.appendChild(dollImg);
-  }
+const dollsContainer = document.querySelector('.dolls-container');
+const dollsData = [
+  { src: 'img/bomb.png', point: -10 },
+  { src: 'img/doll1.png', point: 10 },
+  { src: 'img/doll2.png', point: 20 },
+  { src: 'img/doll3.png', point: 30 },
+];
+const dollCount = 21; // 인형 개수를 22로 변경
+dollsContainer.style.flexWrap = 'wrap'; // flex-wrap을 wrap으로 설정
+for (let i = dollCount - 1; i >= 0; i--) {
+  const randomIndex = Math.floor(Math.random() * dollsData.length);
+  const dollData = dollsData[randomIndex];
+  const doll = document.createElement('img');
+  doll.classList.add('doll');
+  doll.src = dollData.src;
+  doll.setAttribute('point', dollData.point);
+  doll.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`;
+  const randomSize = Math.floor(Math.random() * 60) + 40;
+  doll.style.width = `${randomSize}px`;
+  doll.style.height = `${randomSize}px`;
+  dollsContainer.prepend(doll);
 }
-
-RandomDolls();
