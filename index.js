db07ed0867424fecaa7fb0edcf27f9f2fec5a953
jisilver <jisilver.kim@gmail.com>
2023-03-18 23:35:29 +0900

    design: 아이콘 크기 변경

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const dollsContainer = document.querySelector('.dolls-container');
+
+// 인형 배열 안 객체 생성
 const dollsData = [
-  { src: 'img/bomb.png', point: -10 },
-  { src: 'img/doll1.png', point: 10 },
-  { src: 'img/doll2.png', point: 20 },
-  { src: 'img/doll3.png', point: 30 },
-];
-const dollCount = 21; // 인형 개수를 22로 변경
-dollsContainer.style.flexWrap = 'wrap'; // flex-wrap을 wrap으로 설정
-for (let i = dollCount - 1; i >= 0; i--) {
-  const randomIndex = Math.floor(Math.random() * dollsData.length);
-  const dollData = dollsData[randomIndex];
-  const doll = document.createElement('img');
-  doll.classList.add('doll');
-  doll.src = dollData.src;
-  doll.setAttribute('point', dollData.point);
-  doll.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`;
-  const randomSize = Math.floor(Math.random() * 60) + 40;
-  doll.style.width = `${randomSize}px`;
-  doll.style.height = `${randomSize}px`;
-  dollsContainer.prepend(doll);
+  {src: 'img/bomb.png', point: -10},
+  {src: 'img/doll1.png', point: 10},
+  {src: 'img/doll2.png', point: 20},
+  {src: 'img/doll3.png', point: 30},
+]
+
+function randomDolls(count) {
+  for (let i = 0; i < count; i++) {
+    const index = Math.floor(Math.random() * dollsData.length); // index 랜덤 생성
+    const dollData = dollsData[index]; // dollsData 배열 안에 랜덤 index 넣기
+
+    const doll = document.createElement('img');
+    doll.classList.add('doll'); // 이미지에 doll class 넣기
+    doll.src = dollData.src;
+    doll.setAttribute('data-point', dollsData.point); // 포인트값 저장
+    dollsContainer.appendChild(doll);
+
+    doll.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`; // 랜덤으로 각도 틀기
+
+    const dollSize = Math.floor(Math.random() * 45) + 30; // 0 이상 44이하 정수 + 30 (30 이상 74이하)
+    doll.style.width = `${dollSize}px`;
+  }
 }
+randomDolls(35); // 인형 개수 넘겨주기
+
+
+
