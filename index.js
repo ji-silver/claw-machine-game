98874569297a928818afd2f922c89ddc340156a4
jisilver <jisilver.kim@gmail.com>
2023-03-21 21:08:17 +0900

    fix: 인형 배치 변경

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dollsContainer = document.querySelector('.game__dolls-container');
 
 // 인형 배열 안 객체 생성
 const dollsData = [
-  {src: 'img/bomb.png', point: -10},
+  {src: 'img/bomb.png', point: -50},
   {src: 'img/doll1.png', point: 10},
   {src: 'img/doll2.png', point: 20},
   {src: 'img/doll3.png', point: 30},
@@ -18,16 +18,31 @@ function randomDolls(count) {
     doll.classList.add('doll');
     doll.src = dollData.src;
     doll.setAttribute('data-point', dollData.point);
-    dollsContainer.appendChild(doll);
+
+    if (i < 6) {
+      dollsContainer.insertBefore(doll, dollsContainer.firstChild);
+    } else {
+      const prevDoll = dollsContainer.children[i - 6];
+      dollsContainer.insertBefore(doll, prevDoll.nextSibling);
+    }
 
     // 랜덤 각도 설정
-    doll.style.transform = `rotate(${Math.floor(Math.random() * 361)}deg)`;
-
-
+    doll.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`;
   }
 }
+randomDolls(30); // 인형 개수 넘겨주기
 
-randomDolls(35); // 인형 개수 넘겨주기
 
+// 크레인 멈춤
+const game = document.querySelector('.game')
+const gameCrane = document.querySelector('.game__crane');
 
+game.addEventListener('click', () => {
+  stopCrane();
+});
+
+// 인형 끌어올리기
+function stopCrane() {
+  gameCrane.style.animationPlayState = 'paused';
+}
 
